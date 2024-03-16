9f87458a9235ef479a42f044f8e30fe23c8f7d01
chin kaihin <chinkaihin82@au.com>
2024-03-16 21:18:05 +0900

    222

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,14 @@
 import Image from "next/image";
 import Link from 'next/link'
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <>
-      <h1>Link Test</h1>
-      <ul>
-      <li><a href="#">Page Test</a></li>
-      <li><Link href="about">aboutPage Test</Link></li>
+        about
+        <ul>
+      <li><Link href="/">top</Link></li>
       </ul>
-     
-    </>
       </div>
     </main>
   );
